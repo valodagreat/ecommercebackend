2266225919557bbec85b0eb497e19e53ce9131a1
caleb1-valo <65986150+caleb1-valo@users.noreply.github.com>
2020-12-21 20:26:07 +0100

    added screens

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getProduct, getProducts } from '../controllers/Product.js';
+import { admin, protect } from '../middlewares/auth.js';
+import { createProduct, deleteProduct, getProduct, getProducts, updateProduct } from '../controllers/Product.js';
 const router = express.Router();
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
-router.route('/:id').get(getProduct);
+router.route('/:id').get(getProduct).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct);
 
 export default router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { authUser, createUsers, getUserProfile, getUsers, updateUserProfile } from '../controllers/Users.js';
+import { authUser, createUsers, deleteUser, getUserById, getUserProfile, getUsers, updateUserAdmin, updateUserProfile } from '../controllers/Users.js';
 import { admin, protect } from '../middlewares/auth.js';
 const router = express.Router();
 
 router.route('/').post(createUsers).get(protect, admin, getUsers);
 router.route('/login').post(authUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUserAdmin)
 
 
 
